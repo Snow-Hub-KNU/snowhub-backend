79c2fb888b38f6374a768b12564342dd1a04e823
Seokhun-Yang <ddayang1234@gmail.com>
2023-12-10 01:57:29 +0900

    SkiResortCrawler 클래스 추가

---
+++ src/main/kotlin/com/example/demo/crawler/SkiResortCrawler.kt
@@ -0,0 +1,13 @@
+package com.example.demo.crawler
+
+import com.example.demo.domain.model.SkiResort
+
+class SkiResortCrawler : Crawler {
+    override fun crawl(url: String): SkiResort {
+        TODO()
+    }
+
+    override fun isCrawlable(url: String): Boolean {
+        TODO()
+    }
+}
