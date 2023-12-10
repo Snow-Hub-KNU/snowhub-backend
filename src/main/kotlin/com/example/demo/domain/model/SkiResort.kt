31ed08a8ca9df2e413e04a4a4c1acf4ae5f93818
Seokhun-Yang <ddayang1234@gmail.com>
2023-12-10 20:59:11 +0900

    model(SKiResort) 구조 수정

--- src/main/kotlin/com/example/demo/domain/model/SkiResort.kt
+++ src/main/kotlin/com/example/demo/domain/model/SkiResort.kt
@@ -5,7 +5,5 @@ import java.util.UUID
 data class SkiResort(
         val skiResortId: UUID,
         val resortName: String,
-        val openHours: String,
-        val closeHours: String,
         val slopes: List<Slope> = listOf(),
 )
